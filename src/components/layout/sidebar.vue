eb9fad9190e6a491dbe855db68dfc32eb232336e
jinbo.cai <596310898@163.com>
2023-03-07 17:44:00 +0800

    添加用户管理页面

--- src/components/layout/sidebar.vue
+++ src/components/layout/sidebar.vue
@@ -1,11 +1,21 @@
 <!-- hickey 2023 -->
 <template>
-  <div></div>
+  <el-menu default-active="1" class="el-menu-vertical-demo">
+    <el-menu-item index="1">
+      <el-icon><User /></el-icon>
+      <template #title>用户管理</template>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <el-icon><Aim /></el-icon>
+      <template #title>角色管理</template>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <el-icon><Menu /></el-icon>
+      <template #title>菜单管理</template>
+    </el-menu-item>
+  </el-menu>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-</style>
+<style scoped></style>
--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -1,11 +1,24 @@
 <!-- hickey 2023 -->
 <template>
-  <div>user</div>
+  <el-form :model="form" inline>
+    <el-form-item label="电话号码" prop="phoneNumber">
+      <el-input
+        v-model="form.phoneNumber"
+        placeholder="请输入用户电话号码"
+      ></el-input>
+    </el-form-item>
+    <el-form-item label="用户状态" prop="status">
+      <el-select v-model="form.status" placeholder="请选择用户状态">
+        <el-option value="正常">正常</el-option>
+        <el-option value="停用">停用</el-option>
+      </el-select>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script setup lang="ts">
+const form = reactive({ phoneNumber: undefined, status: undefined });
 
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
