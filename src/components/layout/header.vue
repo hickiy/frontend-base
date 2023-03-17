4fd6f90a1e2b948f907500687c30ef29fcd61f95
hickey <596310898@163.com>
2023-03-17 19:33:03 +0800

    use the prettier formatter in vue3 project at default

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="px-[20px] py-2 flex flex-nowrap justify-between bor border-b border-gray-300">
-    <img class="w-14 rounded-full" src="@/assets/img/favicon.jpg" />
+    <img class="w-14 rounded-full" src="/favicon.jpg" />
     <div class="flex flex-none items-center">
       <el-input class="w-60 mr-8" type="text" prefix-icon="Search"></el-input>
       <el-menu class="border-none" default-active="components" mode="horizontal" :ellipsis="false">
@@ -12,5 +12,5 @@
 </template>
 
 <script setup lang="ts">
-
+  
 </script>
