3f2649cf459f0a0618c9fdf7a3496753d2267b65
hickey <596310898@163.com>
2024-04-07 10:09:06 +0800

    dev: persist the data of pinia store

--- src/store/modules/views.js
+++ src/store/modules/views.js
@@ -4,7 +4,11 @@ const useTagsViewStore = defineStore(
     state: () => ({
       breadcrumbs: [], // 面包屑
       cachedViews: [], // 缓存的视图, 用于keep-alive
-    })
+    }),
+    persist: {
+      storage: window.sessionStorage,
+      paths: ['breadcrumbs'],
+    }
   })
 
 export default useTagsViewStore
