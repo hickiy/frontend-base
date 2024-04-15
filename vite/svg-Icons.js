ff5d813cfa04c05f9d8e3ed8a2a738e8516af5d4
hickey <596310898@163.com>
2024-04-15 21:59:41 +0800

    dev: share the icons for subproject

--- vite/svg-Icons.js
+++ vite/svg-Icons.js
@@ -1,7 +1,35 @@
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import path from 'path';
+import fs from 'fs';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import svgSprite from 'svg-sprite';
+import File from 'vinyl';
+
+
+const config = {
+  mode: {
+    symbol: true // symbol mode to build the SVG
+  }
+};
 
 export default function () {
+  const sprite = new svgSprite(config);
+  const svgFolder = path.join(__dirname, '../src/assets/svg'); // the directory of your SVG files
+
+  fs.readdirSync(svgFolder).forEach(file => {
+    const filePath = path.join(svgFolder, file);
+    const svgContent = fs.readFileSync(filePath, 'utf-8');
+    sprite.add(filePath, file, svgContent);
+  });
+
+  sprite.compile((error, result) => {
+    console.log(result);
+    if (error) {
+      console.error(error);
+    } else {
+      fs.writeFileSync(path.resolve(__dirname, '../dist_/icons.svg'), result.symbol.sprite.contents);
+    }
+  });
+
   return createSvgIconsPlugin({
     iconDirs: [path.resolve(process.cwd(), 'src/assets/svg')],
     symbolId: 'icon-[name]',
